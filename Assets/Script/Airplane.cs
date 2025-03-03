efd94c72cc11e82a1caaa3e057a4a0f244ae783e
LunvrS <149987400+LunvrS@users.noreply.github.com>
2025-03-03 10:52:49 +0700

    Update Script & More
    V0.2

---
+++ Assets/Script/Airplane.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftbooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+            
+            Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
+            rb.AddForce(transform.up * lift.magnitude * liftbooster);
+
+            rb.linearDamping = rb.linearVelocity.magnitude * drag;
+            rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+            
+            //rotation (P Y R)
+
+            float yaw = Input.GetAxis("Horizontal") * yawPower;
+            float pitch = Input.GetAxis("Vertical") * pitchPower;
+            float roll = Input.GetAxis("Roll") * rollPower;
+            
+            rb.AddTorque(transform.up * yaw);
+            rb.AddTorque(transform.right * pitch);
+            rb.AddTorque(transform.forward * roll);
+        }
+
+    }
+}
